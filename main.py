e2cf5b62e2de9912e5b5a6c8ce0f65a939ed708a
Hector <hectorcreatives08@gmail.com>
2023-09-19 15:42:11 -0400

    moving press

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('spaceship.png')
 playerX = 400
 playerY = 300
+playerX_change = 0
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -23,12 +24,20 @@ running = True
 while running:
 
     screen.fill((0, 0, 0))
-    playerX += 0.2
-    print(playerX)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
